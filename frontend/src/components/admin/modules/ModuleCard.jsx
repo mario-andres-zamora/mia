cf71524a1d9e5c0d48fcd3c20f474713b85ff46e
Mario Zamora <mortasoft@gmail.com>
2026-04-07 23:57:04 -0600

    Fix anterior

--- frontend/src/components/admin/modules/ModuleCard.jsx
+++ frontend/src/components/admin/modules/ModuleCard.jsx
@@ -16,6 +16,7 @@ export default function ModuleCard({
     onEditLesson,
     onDeleteLesson,
     onToggleLessonOptional,
+    onOpenLessonEditor,
     onNewResource,
     onEditResource,
     onDeleteResource,
